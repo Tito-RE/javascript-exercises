97e9ffc3771dfe5614198f65a8e69cdf9d5c900f
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-01 10:23:14 -0600

    Add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  let reversedString = "";
+  let textArray = text.split("");
+  for (let i = textArray.length -1; i >= 0; i--) {
+    reversedString += textArray[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
