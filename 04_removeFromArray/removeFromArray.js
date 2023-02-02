5f1d02d8f29faf6e33833d956f126338be1906e5
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-02 08:16:15 -0600

    Add solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(sourceArray,...args) {
+  let newArray = [];
+  sourceArray.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
