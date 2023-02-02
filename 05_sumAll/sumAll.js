3067acc72c1e93586dabd7cd1953293303391a94
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-02 13:40:45 -0600

    Add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+  let sum = 0;
+  let originNumber = min;
+  let endNumber = max;
 
+  if (min < 0 || max < 0) return "ERROR";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (max < min) {
+    originNumber = max;
+    endNumber = min;
+  }
+  
+  for (let i = originNumber; i <= endNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
