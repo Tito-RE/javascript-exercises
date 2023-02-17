a8d289a337f5c07f9ff147b5f983df504426da3b
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-17 09:56:49 -0600

    Add solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  numbers.forEach(element => {
+    sum += element;
+  });	
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let multi = 1;
+  numbers.forEach(element => {
+    multi *= element;
+  });
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for (let i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
