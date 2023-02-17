d8b065f8cc3e1afaa1804343f3d386c8e614f90a
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-17 12:53:32 -0600

    Add solution to excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let status = true;
+  let j = 0;
+  text = cleanText(text);
+  for (let i = text.length-1; i >= 0; i--) {
+    if (text[i] != text[j]) {
+      status = false;
+      break;
+    }
+    j++;
+  }
+  return status;
 };
+
+function cleanText(text) {
+  newText = "";
+  text = text.toLowerCase();
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] != "!" && text[i] != "?" && text[i] != "," && text[i] != "." && text[i] != " ") {
+      newText += text[i];
+    }
+  }
+  return newText;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
