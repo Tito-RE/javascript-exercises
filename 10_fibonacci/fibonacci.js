de50e8398bafbf2394b76e3748e12ebc625420ee
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-18 12:29:16 -0600

    Add solution to excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  input = parseInt(input);
+  let fib;
+  let prev1 = 1;
+  let prev2 = 0;
+  if (input <= 1 && input >= 0) return input;
+  if (input < 0) return "OOPS";
+  
 
+  for (let i = 2; i <= input; i++) {
+    fib = prev2+prev1;
+    prev2 = prev1;
+    prev1 = fib;
+  }
+  return fib;
 };
 
 // Do not edit below this line
