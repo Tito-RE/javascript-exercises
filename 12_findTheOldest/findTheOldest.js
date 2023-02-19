f35370007308b0650e54088422a9c3dba8503763
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-19 10:32:05 -0600

    Add aolution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  people.forEach(element => {
+    if (!element['yearOfDeath']) {
+      element['age'] = 2023 - element.yearOfBirth;
+    } else {
+      element['age'] = element.yearOfDeath - element.yearOfBirth;
+    }
+  });
+  people = people.sort((a,b) => a.age > b.age ? 1: -1);  
+  return people[people.length-1];
 };
 
 // Do not edit below this line
